1e2bec85f258cf6309063c84830c119999577279
Matrixx7777 <73428356+Matrixx7777@users.noreply.github.com>
2023-10-09 11:39:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE hmtl>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+</head>
+<body>
+<style>
+*{
+font-family: sans-serif;
+text-align: center;
+}
+</style>
+<h1> Hello World </h1>
+<?php
+</body>
+</html>
